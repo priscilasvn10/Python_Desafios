879e9fa4db10a087ad1b8bc832e964d7ffed1a5e
priscilasvn10 <priscilasvn10@gmail.com>
2020-11-03 19:30:41 -0300

    Acertar um número entre 0 e 5

--- Desafio028.py
+++ Desafio028.py
@@ -1,8 +1,6 @@
 import random
-
 lista = [0,1,2,3,4,5]
 escolhido = random.choice(lista)
-
 n = int(input('Tente acertar o número entre 0 e 5: '))
 if n==escolhido:
     print('Parabéns você VENCEU!')
