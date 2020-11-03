e12f197868965e395cb8b70c918c7690263b1242
priscilasvn10 <priscilasvn10@gmail.com>
2020-11-03 19:32:31 -0300

    Valor da passagem

--- Desafio031.py
+++ Desafio031.py
@@ -1,4 +1,3 @@
-# calcule o preço da passagem, cobrando 0,50 centavos por km para viagens at´r 200 km e 0,45 centavos para viagens mais longas
 le = float(input('Digite a distância de uma viagem em Km: '))
 if le <= 200:
     valor = le*0.50
